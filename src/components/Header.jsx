6cec2201778b57a595eeee389587618e744b4160
Dorian Guerin <131307954+sixtartix@users.noreply.github.com>
2024-12-23 02:15:32 +0100

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,9 @@ import logo from '../assets/images/logo.jpg';
 import Register from './auth/register';
 import Login from './auth/login';
 import '../styles/header.css';
+import { getThemeForDate } from '../styles/ThemeManager';
 
-function Header() {
+const Header = () => {
     const [user, setUser] = useState(null);
     const [isAdmin, setIsAdmin] = useState(false);
     const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -70,80 +71,81 @@ function Header() {
     };
 
     return (
-        <>
-            <header className="header">
-                <div className="logo">
-                    <img src={logo} alt="Logo Pois Rayures et Caro" className="logo-img" />
-                    <a href="/" className="site-title">
-                        <h1>Pois Rayures et Caro</h1>
-                    </a>
-                </div>
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Logo Pois Rayures et Caro" className="logo-img" />
+                <a href="/" className="site-title">
+                    <h1>
+                        <span style={{ color: 'var(--title-first-part)' }}>Pois Rayures</span>
+                        <span style={{ color: 'var(--title-second-part)' }}> et Caro</span>
+                    </h1>
+                </a>
+            </div>
 
-                <div className="header-center">
-                    <form onSubmit={handleSearch} className={`search-bar ${isSearchExpanded ? 'expanded' : ''}`} ref={searchRef}>
-                        <input
-                            type="search"
-                            placeholder="Rechercher..."
-                            value={searchQuery}
-                            onChange={(e) => setSearchQuery(e.target.value)}
-                            onClick={toggleSearch}
-                        />
-                        <button type="submit">
-                            🔍
-                        </button>
-                    </form>
-                </div>
+            <div className="header-center">
+                <form onSubmit={handleSearch} className={`search-bar ${isSearchExpanded ? 'expanded' : ''}`} ref={searchRef}>
+                    <input
+                        type="search"
+                        placeholder="Rechercher..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onClick={toggleSearch}
+                    />
+                    <button type="submit">
+                        <span className="search-icon">{getThemeForDate().searchIcon}</span>
+                    </button>
+                </form>
+            </div>
 
-                <div className="header-right">
-                    <nav className="nav-buttons">
-                        {user ? (
-                            <>
-                                <a href="#" className="nav-button loyalty-button" disabled>
-                                    Fidélité
+            <div className="header-right">
+                <nav className="nav-buttons">
+                    {user ? (
+                        <>
+                            <a href="#" className="nav-button loyalty-button"  disabled>
+                                Fidélité
+                            </a>
+                            <a href="/ma-liste" className="nav-button">
+                                Ma liste
+                            </a>
+                            <a href="/settings" className="nav-button">
+                                Paramètres
+                            </a>
+                            {isAdmin && (
+                                <a href="/admin" className="nav-button admin-button">
+                                    Administration
                                 </a>
-                                <a href="/ma-liste" className="nav-button">
-                                    Ma liste
-                                </a>
-                                <a href="/settings" className="nav-button">
-                                    Paramètres
-                                </a>
-                                {isAdmin && (
-                                    <a href="/admin" className="nav-button admin-button">
-                                        Administration
-                                    </a>
-                                )}
-                            </>
-                        ) : (
-                            <>
-                                <button className="nav-button" onClick={openLoginModal}>
-                                    Connexion
-                                </button>
-                                <button onClick={openModal} className="nav-button">
-                                    Inscription
-                                </button>
-                            </>
-                        )}
-                    </nav>
-                </div>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            <button className="nav-button" onClick={openLoginModal}>
+                                Connexion
+                            </button>
+                            <button onClick={openModal} className="nav-button">
+                                Inscription
+                            </button>
+                        </>
+                    )}
+                </nav>
+            </div>
 
-                <Modal
-                    isOpen={modalIsOpen}
-                    onRequestClose={closeModal}
-                    contentLabel="Modal d'inscription"
-                >
-                    <Register closeModal={closeModal} />
-                </Modal>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Modal d'inscription"
+            >
+                <Register closeModal={closeModal} />
+            </Modal>
 
-                <Modal
-                    isOpen={loginModalIsOpen}
-                    onRequestClose={closeLoginModal}
-                    contentLabel="Modal de connexion"
-                >
-                    <Login closeModal={closeLoginModal} />
-                </Modal>
-            </header>
-        </>
+            <Modal
+                isOpen={loginModalIsOpen}
+                onRequestClose={closeLoginModal}
+                contentLabel="Modal de connexion"
+            >
+                <Login closeModal={closeLoginModal} />
+            </Modal>
+        </header>
     );
-}
+};
 
 export default Header;
